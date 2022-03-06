1e638a874c5bd3d9d70a47761b32b1dbb791a08e
T9P <shengchun95@live.com>
2022-03-06 13:46:41 +0800

    change input

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+export interface GreeterInput {
+    name: string;
+    greeting?: string;
+}
+
+export const Greeter = (obj: GreeterInput) => {
+    const greeting = obj.greeting || 'Hello';
+    return `${greeting} ${obj.name}`;
+};
